86a6619245000c11dabe27b7d38bc0dd2e72c85e
Deanne Lee <missdeannelee@gmail.com>
2022-05-04 17:11:34 -0600

    very sad attemp revist

---
+++ middle.js
@@ -0,0 +1,17 @@
+
+const middle = function(array) { arr[Math.round((arr.length -2) / 2)];
+ 
+}
+
+
+// find middle in arrays
+
+
+middle([1]) // => []
+middle([1, 2]) 
+
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
