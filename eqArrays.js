656d41d5d326b72ec1a6b0f0636f60935ce9e4ea
Deanne Lee <missdeannelee@gmail.com>
2022-05-04 08:35:30 -0600

    attempt 1

---
+++ eqArrays.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) { 
+  if (actual !== expected) {
+    return ('Assertion Failed `${actual}`');
+  } else 
+  (actual === expected) {
+    return ('Assertion Passed `${expected}`')
+  };
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1) ;
+
+
+const assertEqual = [];
+var eqArrays = []
+
+console.log(assertEqual.includes(eqArrays));
+
+arrayScan = function(assertEqual, eqArrays) {
+  if (let i = 0; i < assertEqual.length; i++)
+  return assertEqual; }
+{  while (let j = 0; j < eqArrays.length; j++)
+  return eqArrays; {
+    compare = function(assertEqual, eqArrays) {
+      if assertEqual !== eqArrays
+      return false
+    } else if assertEqual === eqArrays
+    return true
+  }
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) 
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
