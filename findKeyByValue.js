0fedf3e120b03ea94622d66789b9d0c077fe898e
Deanne Lee <missdeannelee@gmail.com>
2022-05-04 21:37:36 -0600

    i think Im on the right track again asrtEq

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+////implement a function
+const findKey = function(arr1, arr2) {
+  for (const index of object) {
+    console.log(object.keys[0])
+    var key = object.keys[0]
+    if key = false {
+      return 'undefined'
+    }
+  }
+return key[0];
+
+}
+//interate over an object and values
+//return first key [0] and its value
+///if no key given return undefined
+///try implementing object.keys
+//try using a for of loop over the retrn of object.keys
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
